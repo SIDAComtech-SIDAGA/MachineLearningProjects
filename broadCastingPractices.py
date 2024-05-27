5943caa219d5926854a6ffac96fd16d74fd98b8c
sidaga kihongo <sidagawazirikihongo@gmail.com>
2024-05-27 12:43:04 +0300

    Broadcasting and plating with aaray

---
+++ broadCastingPractices.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+# from matplotlib import pyplot as plt
+X = np.random.random((10,3))
+# Y = np.random.randint(10, size=10)
+# dt = pd.DataFrame(Y)
+# dt2 = pd.DataFrame(X)
+# print("Data two")
+# print(dt2)
+# print("Data one dt")
+# print(dt)
+# print(Y)
+# print(X)
+Xmean = X.mean(0)
+print(Xmean)
+Xmean = X.mean()
+print(Xmean)
+
+#And now we can center the X array by substracting the mean(this is the broadcasting operation)
+
+X_centered = X -Xmean
+print("Centered Mean")
+print(X_centered)
+print("Double checking if we have done it correctly")
+print(X_centered.mean(0))
--- coedHustling.py
+++ coedHustling.py
@@ -8,5 +8,36 @@ b = np.array([5,5,5])
 # print(a + 5)
 
 M = np.ones((3,3))
-print(M)
+# print(M)
+# print("\n")
+# print(M + a)
+
+c = np.arange(10)
+print(c)
+
+d = np.arange(10)[:, np.newaxis]
+print(d)
+print(c + d)
+
+print("###Dimensionalism")
+
+e = np.arange(3).reshape((3,1)) + np.arange(3)
+print(e)
+
+M = np.ones((2,3))
+print(M.shape)
+a = np.arange(3)
+print(a.shape)
+print("Broadcasting")
 print(M + a)
+print(a + M)
+
+print("broadcasting 2")
+a2 = a[:, np.newaxis].shape
+print(a.shape)
+print(a2)
+
+print("Broadicasting rules apply to any binary ufunc")
+
+sidaga = np.logaddexp(M,a)
+print(sidaga)
