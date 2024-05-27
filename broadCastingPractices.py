dd00f170ffddc3c43ccfa2b0518711b20dd55f04
sidaga kihongo <sidagawazirikihongo@gmail.com>
2024-05-27 12:53:08 +0300

    Generating random arrays

--- broadCastingPractices.py
+++ broadCastingPractices.py
@@ -23,3 +23,11 @@ print("Centered Mean")
 print(X_centered)
 print("Double checking if we have done it correctly")
 print(X_centered.mean(0))
+
+print("Trying to generate random names")
+# namesdt = np.random.choice(, size=20)
+names = ["Sidaga", "Shein","Shude", "Dorcas", "Baraka"]
+namesdt = np.random.choice(names, size=5)
+print(namesdt)
+nDT = pd.DataFrame(namesdt)
+print(nDT)
