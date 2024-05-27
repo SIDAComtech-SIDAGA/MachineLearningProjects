17cb79d329902865cef50cac7c79322e72962db6
sidaga kihongo <sidagawazirikihongo@gmail.com>
2024-05-27 23:08:47 +0300

    Python operation with Stan

--- BMI.py
+++ BMI.py
@@ -2,26 +2,26 @@
 Weight = int(input('Enter your weight in Kilograms; '))
 # Assigning the variable to the weight 
 a = Weight
-print('Weight: ', a)
+print(f'Weight: ', a)
 
 # Taking height as input from the user 
-Height = float(input("Enter your height in meters; "))
+Height = float(input(f"Enter your height in meters; "))
 # Assigning the variable to the Height
 b = Height
 
 # printing the height to avoid error 
-print("Height: ", b)
+print(f"Height: ", b)
 # using the formula 
 BMI = a/b
 
-print("Body Mass Index (BMI): ", BMI)
+print(f"Body Mass Index (BMI): ", BMI)
 # USING THE IF CONDITION TO DETERMINE WHEATHER THE BME LIES UNDERWEIGHT OR OVERWEIGHT 
 if BMI < 18.5:
-    print("Underweight you need to eat muc stuff ")
+    print(f"Underweight you need to eat muc stuff ")
 elif 18.5 <= BMI <25:
-    print("Normal weight, i really dont know what to do there")
+    print(f"Normal weight, i really dont know what to do there")
 elif 25 <= BMI <30:
-    print("Overweight, start doing exersize every day untili you decrease your weight")
+    print(f"Overweight, start doing exersize every day untili you decrease your weight")
 else:
-    print('Obessse')
+    print(f'Obessse')
     
---
+++ coding_With_Stan.py
@@ -0,0 +1,5 @@
+# SIMPLE MATHEMATICAL OPERATOR 
+
+# 1.division 
+a, b, c, d = 3, 2, 2.0, -3, 10
+print(1/b)
