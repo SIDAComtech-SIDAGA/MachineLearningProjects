5cce22b56e066e9504b4c1945c8d3f0ee027cc05
sidaga kihongo <sidagawazirikihongo@gmail.com>
2024-06-03 17:12:53 +0300

    White Space Stripping

--- WhiteSpaceStriping.py
+++ WhiteSpaceStriping.py
@@ -1,6 +1,14 @@
 import string
 
-Favourite_Language = 'Python '
-print(Favourite_Language)
-Favourite_Language_Stripped = Favourite_Language.rstrip()
-print(Favourite_Language_Stripped)
+# Favourite_Language = 'Python '
+# print(Favourite_Language)
+# Favourite_Language_Stripped = Favourite_Language.rstrip()
+# print(Favourite_Language_Stripped)
+
+
+Favourite_Language = "Python       "
+a = Favourite_Language.lstrip()
+b = Favourite_Language.rsplit()
+
+c = Favourite_Language.strip()
+print(f"a is :{a}, \n b is :{b}, \n c is :{c}")
